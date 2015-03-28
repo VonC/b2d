565acd5d5c9b7983b42d3ca5815fb59e30317d70
VonC <vonc@laposte.net>
2015-03-28 12:26:27 +0100

    Add ..\env.bat, to set right PATH for senv.bat to use

--- senv.bat
+++ senv.bat
@@ -1,12 +1,35 @@
 @echo off
 
+set script=%~dp0%
+set parent=%script: =,%
+set parent=%parent:\= %
+set parentdir=
+call :getparentdir %parent%
+set parent=%parentdir:,= %
+
+if not exist ..\env.bat (
+	echo Add %parent%\env.bat: (..\env.bat^)
+	echo See env.bat.template in %script%
+	echo In ..\env.bat, complete your %%PATH%% with Git, VirtualBox and Boot2Docker
+	exit /B 1
+)
+call "%parent%\env.bat"
+
 rem http://stackoverflow.com/a/7218493/6309: test substring
 echo.%PATH%| findstr /C:"Boot2Docker" 1>nul
-
 if errorlevel 1 (
-  rem http://stackoverflow.com/a/9806979/
-  rem avoid "x86) not expected at this time" error message
-  set "PATH=%PATH%;c:\prgs\Boot2Docker4W"
+	echo "Boot2Docker is not found in PATH: check your ..\env.bat"
+	exit /B 1
+)
+echo.%PATH%| findstr /C:"Git" 1>nul
+if errorlevel 1 (
+	echo "Git is not found in PATH: check your ..\env.bat"
+	exit /B 1
+)
+echo.%PATH%| findstr /C:"VirtualBox" 1>nul
+if errorlevel 1 (
+	echo "VirtualBox is not found in PATH: check your ..\env.bat"
+	exit /B 1
 )
 
 doskey vbm="c:\prgs\VirtualBox\VBoxManage.exe" $*
@@ -14,4 +37,17 @@ doskey vbmmt="c:\prgs\VirtualBox\VBoxManage.exe modifyvm \"boot2docker-vm\" natp
 doskey vbmmu="c:\prgs\VirtualBox\VBoxManage.exe modifyvm \"boot2docker-vm\" natpf1 \"udp-port$1,udp,,$1,,$1\";"
 doskey vbmct="c:\prgs\VirtualBox\VBoxManage.exe controlvm \"boot2docker-vm\" natpf1 \"tcp-port$1,tcp,,$1,,$1\";"
 doskey vbmcu="c:\prgs\VirtualBox\VBoxManage.exe controlvm \"boot2docker-vm\" natpf1 \"udp-port$1,udp,,$1,,$1\";"
-:end
+
+goto :eof
+
+rem http://www.dostips.com/DtTutoFunctions.php#FunctionTutorial.ReturningValuesClassic
+:getparentdir
+if "%~1" EQU "b2d" goto :EOF
+if not "%parentdir%" == "" (
+	set "parentdir=%parentdir%\%~1"
+)
+if "%parentdir%" == "" (
+	set parentdir=%~1
+)
+shift
+goto getparentdir
