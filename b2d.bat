1ba4c7ce48b80ea8a4fd8180ff86b7d5fe3dfe8b
VonC <vonc@laposte.net>
2015-03-29 13:20:12 +0200

    b2d.bat uses profile (which has to be generated by senv)

--- b2d.bat
+++ b2d.bat
@@ -1,5 +1,5 @@
 boot2docker start
-boot2docker ssh sudo cp -f %unixpath%/.profile /var/lib/boot2docker/profile
+boot2docker ssh sudo cp -f %unixpath%/profile /var/lib/boot2docker/profile
 boot2docker ssh sudo /etc/init.d/docker restart
-boot2docker ssh cp -f %unixpath%/.profile .ashrc
+boot2docker ssh cp -f %unixpath%/profile .ashrc
 boot2docker ssh
