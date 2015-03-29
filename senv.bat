dbc93d81cd617328935a6872661b95ee233bb7a4
VonC <vonc@laposte.net>
2015-03-29 13:06:04 +0200

    senv.bat: complete :lowercase for unixpath drive letter

--- senv.bat
+++ senv.bat
@@ -91,4 +91,27 @@ goto getparentdir
 
 :lowercase
 set %~1=!%1:C=c!
+set %~1=!%1:D=d!
+set %~1=!%1:E=e!
+set %~1=!%1:F=f!
+set %~1=!%1:G=g!
+set %~1=!%1:H=h!
+set %~1=!%1:I=i!
+set %~1=!%1:J=j!
+set %~1=!%1:K=k!
+set %~1=!%1:L=l!
+set %~1=!%1:M=m!
+set %~1=!%1:N=n!
+set %~1=!%1:O=o!
+set %~1=!%1:P=p!
+set %~1=!%1:Q=q!
+set %~1=!%1:R=r!
+set %~1=!%1:S=s!
+set %~1=!%1:T=t!
+set %~1=!%1:U=u!
+set %~1=!%1:V=v!
+set %~1=!%1:W=w!
+set %~1=!%1:X=w!
+set %~1=!%1:Y=y!
+set %~1=!%1:Z=z!
 goto :eof
