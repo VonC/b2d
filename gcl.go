a3641359a93f53550c8ab7e97c4f256f53db4ebe
VonC <vonc@laposte.net>
2015-04-20 13:29:40 +0200

    gcl.go: factorize cmd execution

--- gcl.go
+++ gcl.go
@@ -6,26 +6,34 @@ import "log"
 import "os/exec"
 import "strings"
 
+func cmd(cmd string) string {
+	out, err := exec.Command("sh", "-c", cmd).Output()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("out='%s', err='%s'", out, err))
+	}
+	return string(out)
+}
+
 // docker run --rm -i -t -v `pwd`:`pwd` -w `pwd` --entrypoint="/bin/bash" go -c 'go build gcl.go'
 func main() {
 	args := os.Args
 	id := args[1]
 	fmt.Printf("container id: %s\n", id)
-	out, err := exec.Command("sh", "-c", fmt.Sprintf("docker inspect -f '{{ .Name }}' %s", id)).Output()
-	if err != nil {
-		log.Fatal(fmt.Sprintf("out='%s', err='%s'", out, err))
-	}
-	name := strings.TrimSpace(string(out))[1:]
+	out := cmd(fmt.Sprintf("docker inspect -f '{{ .Name }}' %s", id))
+	name := strings.TrimSpace(out)[1:]
 	fmt.Printf("  name: '%s'\n", name)
-	out, err = exec.Command("sh", "-c", fmt.Sprintf("docker inspect -f '{{ range $key, $value := .Volumes }}{{ $key }},{{ $value }}##~#{{ end }}' %s", name)).Output()
-	if err != nil {
-		log.Fatal(fmt.Sprintf("out='%s', err='%s'", out, err))
-	}
-	volumes := strings.Split(string(out), "##~#")
+	out = cmd(fmt.Sprintf("docker inspect -f '{{ range $key, $value := .Volumes }}{{ $key }},{{ $value }}##~#{{ end }}' %s", name))
+	volumes := strings.Split(out, "##~#")
 	volumes = volumes[:len(volumes)-1]
 	fmt.Printf("    volumes: '%v': %d\n", volumes, len(volumes))
 	for i, volume := range volumes {
 		vols := strings.Split(volume, ",")
-		fmt.Printf("      volume %d: '%v'=>'%v'\n", i, vols[0], vols[1])
+		path := vols[0]
+		vfs := vols[1]
+		fmt.Printf("      volume %d: '%v'=>'%v'\n", i, path, vfs)
+		if strings.Contains(vfs, "/var/lib/docker/vfs/dir/") {
+			link := name + "###" + strings.Replace(path, "/", ",#,", -1)
+			fmt.Printf("      link '%s'\n", link)
+		}
 	}
 }
