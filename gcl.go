7c3c55f3e056929f4b1506d435a32db67ab80c6d
VonC <vonc@laposte.net>
2015-04-23 14:44:57 +0200

    gcl.go: read containers

--- gcl.go
+++ gcl.go
@@ -56,8 +56,21 @@ type container struct {
 	name    string
 	id      string
 	stopped bool
-	volumes []volume
+	volumes []*volume
 }
+
+func (c *container) trunc() string {
+	if len(c.id) > 0 {
+		return c.id[:7]
+	}
+	return ""
+}
+
+func (c *container) String() string {
+	return "cnt '" + c.name + "' (" + c.trunc() + ")" + fmt.Sprintf("[%v] - %d vol", c.stopped, len(c.volumes))
+}
+
+var containers = []*container{}
 
 func mustcmd(acmd string) string {
 	out, err := cmd(acmd)
@@ -131,9 +144,42 @@ func readVolumes() {
 	fmt.Printf("volumes: %v\nmarkers: %v\n", volumes, markers)
 }
 
+func readContainer() {
+
+	out := mustcmd("docker ps -aq --no-trunc")
+	contlines := strings.Split(out, "\n")
+	fmt.Println(contlines)
+	for _, contline := range contlines {
+		id := contline
+		res := mustcmd("docker inspect -f '{{ .Name }},{{ range $key, $value := .Volumes }}{{ $key }},{{ $value }}##~#{{ end }}' " + id)
+		fmt.Println("res1: '" + res + "'")
+		name := res[1:strings.Index(res, ",")]
+		cont := &container{name: name, id: id}
+		res = res[strings.Index(res, ",")+1:]
+		fmt.Println("res2: '" + res + "'")
+		vols := strings.Split(res, "##~#")
+		fmt.Println(vols)
+		for _, vol := range vols {
+			elts := strings.Split(vol, ",")
+			if len(elts) == 2 {
+				fmt.Printf("elts: '%v'\n", elts)
+				vd := newvdir(filepath.Base(elts[1]))
+				if vd == "" {
+					fmt.Printf("Invalid volume folder detected: '%s'\n", elts[1])
+					break
+				}
+				cont.volumes = append(cont.volumes, &volume{dir: vd})
+			}
+		}
+		containers = append(containers, cont)
+	}
+	fmt.Printf("containers: %v\n", containers)
+}
+
 // docker run --rm -i -t -v `pwd`:`pwd` -w `pwd` --entrypoint="/bin/bash" go -c 'go build gcl.go'
 func main() {
 	readVolumes()
+	readContainer()
 	os.Exit(0)
 	out := mustcmd("docker ps -qa --no-trunc")
 	containers := strings.Split(out, "\n")
