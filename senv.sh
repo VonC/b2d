435d18961fa45af81e74a251ac89ac96073aa5c9
VonC <vonc@laposte.net>
2015-06-24 14:33:18 +0200

    senv.sh

---
+++ senv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo $scriptdir
+parent=$(dirname ${scriptdir})
+echo $parent
+set -a 
+source ../env.bat
+set +a
+git -C $scriptdir config filter.dffilter.smudge ${scriptdir}/dfsmudge.sh
+git -C $scriptdir config filter.dffilter.clean ${scriptdir}/dfclean.sh
+cp -f ${scriptdir}/dfsmudge.sh.template ${scriptdir}/dfsmudge.sh
+chmod +x ${scriptdir}/dfsmudge.sh
+
+if [[ ! -z ${http_proxy} ]]
+	then
+    sed -i -e "s/#hasproxy#/1/g" dfsmudge.sh
+	sed -i -e "s;#http_proxy#;${http_proxy};g" dfsmudge.sh
+	sed -i -e "s;#https_proxy#;${https_proxy};g" dfsmudge.sh
+	sed -i -e "s;#no_proxy#;${no_proxy};g" dfsmudge.sh
+fi
+
+sed -i -e "s;_unixpath_;${scriptdir};g" dfsmudge.sh
+echo $HOME
+export HOME=${HOME}
+
