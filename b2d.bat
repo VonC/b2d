65079de4144aff0c8b26915050f8e9504c82bdd9
VonC <vonc@laposte.net>
2015-08-10 21:18:37 +0200

    b2d.bat: copies .bash_aliases in HOME*

--- b2d.bat
+++ b2d.bat
@@ -3,4 +3,5 @@ boot2docker start
 boot2docker ssh sudo cp -f %unixpath%/profile /var/lib/boot2docker/profile
 boot2docker ssh sudo /etc/init.d/docker restart
 boot2docker ssh cp -f %unixpath%/profile .ashrc
+boot2docker ssh cp -f %unixpath%/.bash_aliases .bash_aliases
 boot2docker ssh
