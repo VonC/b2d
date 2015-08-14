93ccc146425e62692fd81ee96ba82d6b48c3ca72
VonC <vonc@laposte.net>
2015-08-14 15:01:54 +0200

    b2d.bat: reads port numbers from envs/*, update vm port forwarding rules

--- b2d.bat
+++ b2d.bat
@@ -3,19 +3,14 @@ boot2docker start
 
 REM already running means controlvm (as opposed to modifyvm)
 
-VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port80,tcp,,80,,80"; 2> NUL
-VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port80,udp,,80,,80"; 2> NUL
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port443,tcp,,443,,443"; 2> NUL
-VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port443,udp,,443,,443"; 2> NUL
-
-VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port8443,tcp,,8443,,8443"; 2> NUL
-VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port8443,udp,,8443,,8443"; 2> NUL
-
-VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port8543,tcp,,8543,,8543"; 2> NUL
-VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port8543,udp,,8543,,8543"; 2> NUL
-VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port8553,tcp,,8553,,8553"; 2> NUL
-VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port8553,udp,,8553,,8553"; 2> NUL
+for /f "tokens=2 delims==" %%a in ('findstr PORT envs\*') do (
+	set p=%%a
+	set p=!p: =!
+	VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port!p!,tcp,,!p!,,!p!"; 2> NUL
+	VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port!p!,udp,,!p!,,!p!"; 2> NUL
+)
 
 boot2docker ssh sudo cp -f %unixpath%/profile /var/lib/boot2docker/profile
 boot2docker ssh sudo /etc/init.d/docker restart
