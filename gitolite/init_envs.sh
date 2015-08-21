5087ab207944c470fcd907f212b814b132b9d2c2
VonC <vonc@laposte.net>
2015-08-21 11:19:04 +0200

    gitolite/init_envs.sh: more verbose when exit early

--- gitolite/init_envs.sh
+++ gitolite/init_envs.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [[ ! -e "${H}/.envs.private" ]] ; then exit 0 ; fi
+H=/home/git
+
+if [[ ! -e "${H}/.envs.private" ]] ; then
+  echo "no /home/git/.envs.private"
+  exit 0 ;
+fi
 
 source "${H}/sbin/usrcmd/get_tpl_value"
 
 get_tpl_value "${H}/.envs.private" "@UPSTREAM_URL_HGIT@" upstream_url
 get_tpl_value "${H}/.envs.private" "@UPSTREAM_NAME@" upstream_name
 
-if [[ "${upstream_url}" == "" || "${upstream_name}" == "" ]] ; then exit 0 ; fi
+if [[ "${upstream_url}" == "" || "${upstream_name}" == "" ]] ; then
+  echo "no upstream_url ('${upstream_url}') or upstream_name ('${upstream_name}')"
+  exit 0 ;
+fi
 
 # A login must be defined for pushing gitolite admin repo
 get_tpl_value "${H}/.envs.private" "@USER_GA_PUSH@" user_ga_push
@@ -21,7 +29,6 @@ upstream_url="${upstream_url#*@}"
 upstream_url="https://${user_ga_push}@${upstream_url#https://}"
 
 export GIT_DIR="${H}/repositories/gitolite-admin.git"
-export xxgit=1
 
 r=$(git remote show -n ${upstream_name}|grep "https")
 
@@ -37,6 +44,7 @@ if [[ "${r}" == "" ]] ; then
   git remote set-url ${upstream_name} ${upstream_url}gitolite-admin
 fi
 
+# TODO to export in an runtime ini script
 if [[ -e "${H}/.gnupg/users.netrc.asc" ]]; then
   chelper=$(git config --local --get credential.helper)
   if [[ "${chelper}" == "" || "${chelper#netrc -}" == "${chelper}" ]] ; then
@@ -47,5 +55,4 @@ fi
 gtl="${H}/gitolite"
 ln -fs "../../../gitolite/post-receive-gitolite-admin" "${H}/repositories/gitolite-admin.git/hooks/post-receive"
 
-unset xxgit
 unset GIT_DIR
