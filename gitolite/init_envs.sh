441cff738cd3954a841cfdf0c5b3e6f30e3829af
VonC <vonc@laposte.net>
2015-08-25 16:19:21 +0200

    gitolite/init_envs.sh: register upstream_name and apache.upstream.cont

--- gitolite/init_envs.sh
+++ gitolite/init_envs.sh
@@ -2,6 +2,25 @@
 
 H=/home/git
 
+function update_remote() {
+  a_remote_name="${1}"
+  a_remote_url="${2}"
+  a_push_user="${3}"
+  r=$(git config --local remote.${a_remote_name}.url|grep "https")
+
+  if [[ "${r}" == "" ]] ; then
+    echo "register '${a_remote_name}' as '${a_remote_url}gitolite-admin'"
+    git remote add ${a_remote_name} ${a_remote_url}gitolite-admin
+  fi
+
+  r=$(git config --local remote.${a_remote_name}.url|grep "https"|grep "${a_push_user}@")
+
+  if [ "${r}" = "" ] || [ "${r#${a_remote_url}}" = "${r}" ] ; then
+    echo "update '${a_remote_name}' as '${a_remote_url}gitolite-admin'"
+    git remote set-url ${a_remote_name} ${a_remote_url}gitolite-admin
+  fi
+}
+
 if [[ ! -e "${H}/.envs.private" ]] ; then
   echo "no /home/git/.envs.private"
   exit 0 ;
@@ -30,19 +49,8 @@ upstream_url="https://${user_ga_push}@${upstream_url#https://}"
 
 export GIT_DIR="${H}/repositories/gitolite-admin.git"
 
-r=$(git remote show -n ${upstream_name}|grep "https")
-
-if [[ "${r}" == "" ]] ; then
-  echo "register '${upstream_name}' as '${upstream_url}gitolite-admin'"
-  git remote add ${upstream_name} ${upstream_url}gitolite-admin
-fi
-
-r=$(git remote show -n ${upstream_name}|grep "https"|grep "${user_ga_push}@")
-
-if [[ "${r}" == "" ]] ; then
-  echo "update '${upstream_name}' as '${upstream_url}gitolite-admin'"
-  git remote set-url ${upstream_name} ${upstream_url}gitolite-admin
-fi
+update_remote "${upstream_name}" "${upstream_url}" "${user_ga_push}"
+update_remote "apache.upstream.cont" "${upstream_url}" "${user_ga_push}"
 
 # TODO to export in an runtime ini script
 if [[ -e "${H}/.gnupg/users.netrc.asc" ]]; then
