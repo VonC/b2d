fb3ddae5c9710417f016aefb4b63b3cad55b7b8f
VonC <vonc@laposte.net>
2015-11-22 22:30:44 +0100

    b2d.bat: use docker-machine commands

--- b2d.bat
+++ b2d.bat
@@ -1,10 +1,10 @@
 @echo off
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-for /f "delims=" %%A in ('boot2docker status') do set "status=%%A"
+for /f "delims=" %%A in ('docker-machine status default') do set "status=%%A"
 echo.%status%
 if "%status%" NEQ "running" (
-	boot2docker start
+	docker-machine start default
 )
 
 REM already running means controlvm (as opposed to modifyvm)
@@ -13,12 +13,10 @@ REM already running means controlvm (as opposed to modifyvm)
 for /f "tokens=2 delims==" %%a in ('findstr PORT envs\*') do (
 	set p=%%a
 	set p=!p: =!
-	VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port!p!,tcp,,!p!,,!p!"; 2> NUL
-	VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port!p!,udp,,!p!,,!p!"; 2> NUL
+	VBoxManage controlvm "default" natpf1 "tcp-port!p!,tcp,,!p!,,!p!"; 2> NUL
+	VBoxManage controlvm "default" natpf1 "udp-port!p!,udp,,!p!,,!p!"; 2> NUL
 )
 
-boot2docker ssh sudo cp -f %unixpath%/profile /var/lib/boot2docker/profile
-boot2docker ssh sudo /etc/init.d/docker restart
-boot2docker ssh cp -f %unixpath%/profile .ashrc
-boot2docker ssh cp -f %unixpath%/.bash_aliases .bash_aliases
-boot2docker ssh
+docker-machine ssh default cp -f %unixpath%/profile .ashrc
+docker-machine ssh default cp -f %unixpath%/.bash_aliases .bash_aliases
+docker-machine ssh default
