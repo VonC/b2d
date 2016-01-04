32f3f2c6baeceaba63c967080b6e070095e7c1f1
VonC <vonc@laposte.net>
2016-01-04 15:07:28 +0100

    b2d.bat: only controlvm is mv was stopped and just restarted

--- b2d.bat
+++ b2d.bat
@@ -5,16 +5,15 @@ for /f "delims=" %%A in ('docker-machine status default') do set "status=%%A"
 echo.%status%
 if "%status%" NEQ "running" (
 	docker-machine start default
-)
 
-REM already running means controlvm (as opposed to modifyvm)
+	REM already running means controlvm (as opposed to modifyvm)
+	for /f "tokens=2 delims==" %%a in ('findstr PORT envs\*') do (
+		set p=%%a
+		set p=!p: =!
+		VBoxManage controlvm "default" natpf1 "tcp-port!p!,tcp,,!p!,,!p!"; 2> NUL
+		VBoxManage controlvm "default" natpf1 "udp-port!p!,udp,,!p!,,!p!"; 2> NUL
+	)
 
-
-for /f "tokens=2 delims==" %%a in ('findstr PORT envs\*') do (
-	set p=%%a
-	set p=!p: =!
-	VBoxManage controlvm "default" natpf1 "tcp-port!p!,tcp,,!p!,,!p!"; 2> NUL
-	VBoxManage controlvm "default" natpf1 "udp-port!p!,udp,,!p!,,!p!"; 2> NUL
 )
 
 docker-machine ssh default cp -f %unixpath%/profile .ashrc
