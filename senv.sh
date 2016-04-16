c20beee237460aac4e0725b87dc4bcb13748677b
VonC <vonc@laposte.net>
2016-04-16 20:34:19 +0200

    . ./senv.sh works if b2d used from Linux directly

--- senv.sh
+++ senv.sh
@@ -1,33 +1,34 @@
 #!/bin/ash
 
 echo "PATH=${PATH}"
-scriptdir=$( cd "$( dirname . )" && pwd )
-echo "scriptdir='${scriptdir}'"
-echo $PATH|grep -E ":${scriptdir}($|:)"
+b2d=$( cd "$( dirname . )" && pwd )
+echo "b2d='${b2d}'"
+bindir=${b2d}/bin
+echo $PATH|grep -E ":${bindir}($|:)"
 if [ $? -ne 0 ]; then
 	echo "add path"
-	export PATH=$PATH:${scriptdir}
+	export PATH=$PATH:${bindir}
 fi
-parent=$(dirname ${scriptdir})
+parent=$(dirname ${b2d})
 echo "parent='${parent}'"
 set -a 
-if [ -e ../env.sh ]; then . ../env.sh; fi
+if [ -e ${parent}/env.sh ]; then . ${parent}/env.sh; fi
 set +a
-git -C $scriptdir config filter.dffilter.smudge ${scriptdir}/dfsmudge.sh
-git -C $scriptdir config filter.dffilter.clean ${scriptdir}/dfclean.sh
-/bin/cp -f ${scriptdir}/dfsmudge.sh.template ${scriptdir}/dfsmudge.sh
-chmod +x ${scriptdir}/dfsmudge.sh
+git -C ${b2d} config filter.dffilter.smudge ${bindir}/dfsmudge.sh
+git -C ${b2d} config filter.dffilter.clean ${bindir}/dfclean.sh
+/bin/cp -f ${bindir}/dfsmudge.sh.template ${bindir}/dfsmudge.sh
+chmod +x ${bindir}/dfsmudge.sh
 
 if [[ ! -z ${http_proxy} ]]
 	then
-    sed -i -e "s/#hasproxy#/1/g" dfsmudge.sh
-	sed -i -e "s;#http_proxy#;${http_proxy};g" dfsmudge.sh
-	sed -i -e "s;#https_proxy#;${https_proxy};g" dfsmudge.sh
-	sed -i -e "s;#no_proxy#;${no_proxy};g" dfsmudge.sh
+    sed -i -e "s/#hasproxy#/1/g" ${bindir}/dfsmudge.sh
+	sed -i -e "s;#http_proxy#;${http_proxy};g" ${bindir}/dfsmudge.sh
+	sed -i -e "s;#https_proxy#;${https_proxy};g" ${bindir}/dfsmudge.sh
+	sed -i -e "s;#no_proxy#;${no_proxy};g" ${bindir}/dfsmudge.sh
 fi
 
-sed -i -e "s;_unixpath_;${scriptdir};g" dfsmudge.sh
-echo $HOME
+sed -i -e "s;_unixpath_;${b2d};g" ${bindir}/dfsmudge.sh
 export HOME=${parent}
-export PATH=$PATH:${scriptdir}
-. .bash_aliases
+echo "HOME='$HOME'"
+# export PATH=$PATH:${scriptdir}
+. ${b2d}/scripts/.bash_aliases
