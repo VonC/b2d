5d4e0e2f0202329f13b00991e0a1708031ea0a57
VonC <vonc@laposte.net>
2016-04-17 04:16:46 +0200

    nodes/kv/senv.bat: makes sure calls to a bat does not stop

--- nodes/kv/senv.bat
+++ nodes/kv/senv.bat
@@ -2,13 +2,13 @@
 setlocal enabledelayedexpansion
 
 if not exist %USERPROFILE%\.docker\machine\machines\kv (
-	dmcv kv
+	call dmcv kv
 )
 docker-machine ls kv
 FOR /F "tokens=* USEBACKQ" %%F IN (`dm ssh kv "if [ ^! -e /var/lib/boot2docker/bootsync.sh ]; then echo nobootsync; fi"`) DO (SET checkbootsync=%%F)
 if "%checkbootsync%"=="nobootsync" (
 	echo fix IP for kv at 99.100
-	dmvbf kv 99 100
+	call dmvbf kv 99 100
 	dm regenerate-certs -f kv
 	dm restart kv
 )
