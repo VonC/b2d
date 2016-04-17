e4f3cbea1e4604a53a72303d7d337b7d12cc8ae1
VonC <vonc@laposte.net>
2016-04-17 02:54:25 +0200

    gen_ssl_key_and_crt.sh generates a .o.cnf with DNS:machine and IP:ip

--- nodes/kv/gen_ssl_key_and_crt.sh
+++ nodes/kv/gen_ssl_key_and_crt.sh
@@ -9,6 +9,11 @@ if [[ ! -e ${certs} ]]; then mkdir -p ${certs}; fi
 a_hostname=localhost
 a_fqn=localhost
 
+machine=$(hostname)
+# http://unix.stackexchange.com/questions/119269/how-to-get-ip-address-using-shell-script
+ip=$(ifconfig eth1 | awk '/inet addr/{print substr($2,6)}')
+echo "machine='${machine}', ip='${ip}'"
+
 # if no private certificate was given, generate self-signed one locally
 
 fqnpassword="${a_fqn}1";
@@ -16,13 +21,16 @@ passphrasekey="${certs}/${a_fqn}.passphrase.key"
 key="${certs}/${a_fqn}.key"
 cert="${certs}/${a_fqn}.crt"
 cnf="${DIR}/o.cnf"
+dcnf="${DIR}/.o.cnf"
+sed "s/#san#/DNS:localhost,DNS:${machine},IP:${ip}/" ${cnf}>"${dcnf}"
+
 #cnf="${certs}/openssl.cnf"
 ext="v3_ca"
 #ext="v3_req"
 if [[ ! -e "${passphrasekey}" ]]; then
   openssl genrsa -des3 -passout pass:${fqnpassword} -out "${passphrasekey}" 1024
   openssl rsa -passin pass:${fqnpassword} -in "${passphrasekey}" -out "${key}"
-  openssl req -new -config "${cnf}" -extensions "${ext}" -x509 -days 730 -key "${key}" -out "${cert}"
+  openssl req -new -config "${dcnf}" -extensions "${ext}" -x509 -days 730 -key "${key}" -out "${cert}"
   # cat "${cert}" >> "${HOME}/.ssh/curl-ca-bundle.crt"
 fi
 cp -f "${key}" "${certs}/key"
