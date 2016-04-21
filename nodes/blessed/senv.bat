affe895835512059e1a79109c87ca7cb84addc9b
VonC <vonc@laposte.net>
2016-04-21 20:03:15 +0200

    nodes senv.bat: fix ipkv, ls machine only when creating machine

--- nodes/blessed/senv.bat
+++ nodes/blessed/senv.bat
@@ -4,15 +4,15 @@ setlocal enabledelayedexpansion
 set machine=blessed
 if not exist %USERPROFILE%\.docker\machine\machines\%machine% (
 	call dmcv %machine% kv
+	docker-machine ls %machine%
 )
-docker-machine ls %machine%
-for /f "delims=" %%A in ('docker-machine ip kv') do set "ipkv=%%A"
-echo ipkv='%ipkv%'
 FOR /F "tokens=* USEBACKQ" %%F IN (`dm ssh %machine% "if [ ^! -e /var/lib/boot2docker/bootsync.sh ]; then echo nobootsync; fi"`) DO (SET checkbootsync=%%F)
 if "%checkbootsync%"=="nobootsync" (
+	for /f "delims=" %%A in ('docker-machine ip kv') do set "ipkv=%%A"
+	echo ipkv='!ipkv!'
 	echo fix IP for %machine% at 99.101
 	call dmvbf %machine% 99 101
-	docker-machine ssh %machine% "sudo sh -c 'echo \"echo %ipkv% kv^>^>/etc/hosts\" | sudo tee -a /var/lib/boot2docker/bootsync.sh >/dev/null'"
+	docker-machine ssh %machine% "sudo sh -c 'echo \"echo !ipkv! kv^>^>/etc/hosts\" | sudo tee -a /var/lib/boot2docker/bootsync.sh >/dev/null'"
 	dm regenerate-certs -f %machine%
 	dm restart %machine%
 )
--- nodes/staging/senv.bat
+++ nodes/staging/senv.bat
@@ -1,18 +1,18 @@
-@echo off
+rem @echo off
 setlocal enabledelayedexpansion
 
 set machine=staging
 if not exist %USERPROFILE%\.docker\machine\machines\%machine% (
 	call dmcv %machine% kv
+	docker-machine ls %machine%
 )
-docker-machine ls %machine%
-for /f "delims=" %%A in ('docker-machine ip kv') do set "ipkv=%%A"
-echo ipkv='%ipkv%'
 FOR /F "tokens=* USEBACKQ" %%F IN (`dm ssh %machine% "if [ ^! -e /var/lib/boot2docker/bootsync.sh ]; then echo nobootsync; fi"`) DO (SET checkbootsync=%%F)
 if "%checkbootsync%"=="nobootsync" (
+	for /f "delims=" %%A in ('docker-machine ip kv') do set "ipkv=%%A"
+	echo ipkv='!ipkv!'
 	echo fix IP for %machine% at 99.102
 	call dmvbf %machine% 99 102
-	docker-machine ssh %machine% "sudo sh -c 'echo \"echo %ipkv% kv^>^>/etc/hosts\" | sudo tee -a /var/lib/boot2docker/bootsync.sh >/dev/null'"
+	docker-machine ssh %machine% "sudo sh -c 'echo \"echo !ipkv! kv^>^>/etc/hosts\" | sudo tee -a /var/lib/boot2docker/bootsync.sh >/dev/null'"
 	dm regenerate-certs -f %machine%
 	dm restart %machine%
 )
