d546e5e3311eb1ae3de10e17591aa22e378ca714
VonC <vonc@laposte.net>
2016-04-21 20:05:17 +0200

    nodes\kv\senv.bat: ls-machine when creating machine

--- nodes/kv/senv.bat
+++ nodes/kv/senv.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 if not exist %USERPROFILE%\.docker\machine\machines\kv (
 	call dmcv kv kv
 	rm -Rf certs
+	docker-machine ls kv
 )
-docker-machine ls kv
 FOR /F "tokens=* USEBACKQ" %%F IN (`dm ssh kv "if [ ^! -e /var/lib/boot2docker/bootsync.sh ]; then echo nobootsync; fi"`) DO (SET checkbootsync=%%F)
 if "%checkbootsync%"=="nobootsync" (
 	echo fix IP for kv at 99.100
